3e0a0dae450c0c681a43a6cf3cfba93f2f6f2cff
Melisa2109 <melutorrez@gmail.com>
2022-07-14 17:55:13 -0300

    actualizaciones media queries

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const menuItems = documen.queryselectorAll(".menu-item");
+const menuBtn = document.querySelector("#menu-button");
+
+menuBtn.addEventListener("click")
+
+menuItems.forEach(function(item) {
+    const currentItem = document.querySelector(".active")
+}
+    
+});
