973753cbbf46df9da9fe52faef982307d4249b98
Melisa2109 <melutorrez@gmail.com>
2022-07-16 09:51:54 -0300

    intento de media

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
-const menuItems = documen.queryselectorAll(".menu-item");
+const menuItem = document.querySelectorAll("menu-item");
 const menuBtn = document.querySelector("#menu-button");
 
-menuBtn.addEventListener("click")
+menuBtn.addEventListener("click", function(){
+    document.body.classList.toggle("mobile-menu-active");
+});
 
-menuItems.forEach(function(item) {
-    const currentItem = document.querySelector(".active")
-}
-    
+menuItem.forEach(function(item) {
+
+    item.addEventListener("click", function() {
+        
+        document.body.classList.remove("mobile-menu-active");
+        const currentItem = document.querySelector("active");
+        currentItem.classList.remove("active");
+        this.classList.add("active")
+    });
 });
