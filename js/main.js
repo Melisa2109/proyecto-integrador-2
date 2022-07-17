2cb538389a95a5a6dfb8b8d4930fddd24e6e2687
Melisa2109 <melutorrez@gmail.com>
2022-07-17 20:03:25 -0300

    actualizaciones

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
-const menuItem = document.querySelectorAll("menu-item");
-const menuBtn = document.querySelector("#menu-button");
+const menuBtn = document.querySelector('#menu-button');
+const menuItems = document.querySelectorAll(".menu-item");
 
-menuBtn.addEventListener("click", function(){
-    document.body.classList.toggle("mobile-menu-active");
+menuBtn.addEventListener('click', function() {
+    document.body.classList.toggle('mobile-menu-active');
 });
 
-menuItem.forEach(function(item) {
+menuItems.forEach(function(item) {
 
-    item.addEventListener("click", function() {
-        
-        document.body.classList.remove("mobile-menu-active");
-        const currentItem = document.querySelector("active");
-        currentItem.classList.remove("active");
-        this.classList.add("active")
+    item.addEventListener('click', function() {
+
+    document.body.classList.remove('mobile-menu-active');
+
+    const currentItem = document.querySelector('.active');
+    currentItem.classList.remove('active');
+    this.classList.add('active');
     });
 });
